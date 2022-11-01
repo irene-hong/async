0499afe0f632fefc14310326597011cf0c4d7978
irenehong <irene_hong@outlook.com>
2022-11-01 10:21:14 -0400

    caifeih

--- Repos_Async_Class_Starter.playground/Contents.swift
+++ Repos_Async_Class_Starter.playground/Contents.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 import Foundation
-
+// let multiply = {(a: Int, b: int) -> Int in a * b}
 // Simplified decodable structs from earlier exercise
 struct Repositories: Decodable {
   let repos: [Repository]
@@ -21,21 +21,54 @@ struct Repository: Decodable {
 }
 
 // Our initial function (needs fixing...)
-func fetchRepositories() throws -> [Repository] {
+func fetchRepositories() async throws -> [Repository] {
   let url = URL(string: "https://api.github.com/search/repositories?q=language:swift&sort=stars&order=desc")!
-  let (data, _) = try URLSession.shared.data(from: url)
-  return try JSONDecoder().decode(Repositories.self, from: data).repos
+  let (data, _) = try await URLSession.shared.data(from: url)
+  return try await JSONDecoder().decode(Repositories.self, from: data).repos
 }
 
+/** basic try-catch
+do {
+  let res = try somemethod()
+} catch {
+
+}
+ */
+
+/**
+ define error
+ enum EncryptionError: Error {
+   case empty
+   case short
+ }
+ // in somemethod
+ guard password.count >= 5 else {
+   throw EncryptionError.short
+ }
+// in main method
+ catch EncryptionError.empty {
+   print("....")
+ }
+ */
 
 // A task to utilize this function
 // - call function then loop over repos array to print out the name & url for each
 // - interject lots of print statements with 'Step X' to see how things are progressing
 Task {
   print("Step 1")
-
-  // ...
-
-  print("Step 5")
+  print("Start task")
+  // fetch data
+  do {
+    let data = try await fetchRepositories();
+    // print data
+    print("Step 2")
+    print(data)
+    // finish the task
+    print("End task")
+    print("Step 5")
+  } catch {
+    print("Error")
+  }
+  
 }
 print("Step 6")
